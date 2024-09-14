6bd15257365606060c0450cdddd04fb58330e365
Karol <11367@kasprzak.edu.pl>
2024-09-14 18:34:39 +0200

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace rpg_character_creator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace rpg_character_creator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Treats.Items.Add("Strength");
+            Treats.Items.Add("Intelligence");
+            Treats.Items.Add("Speed");
+            Treats.Items.Add("Durability");
+
+            Skills.Items.Add("Sword throw");
+            Skills.Items.Add("Magic spell");
+            Skills.Items.Add("Turbo");
+            Skills.Items.Add("Magic armor");
+
+            Races.Items.Add("Human");
+            Races.Items.Add("Reptilian");
+            Races.Items.Add("Elf");
+            Races.Items.Add("Furry");
+        }
+    }
+}
