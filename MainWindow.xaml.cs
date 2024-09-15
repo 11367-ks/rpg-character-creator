6266bbbf85df67f2714a2b4037ac8e12631f61fd
Karol <11367@kasprzak.edu.pl>
2024-09-15 13:35:23 +0200

    Code update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,13 +7,15 @@ namespace rpg_character_creator
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> traits = new List<string>();
+        List<string> skills = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
-            Treats.Items.Add("Strength");
-            Treats.Items.Add("Intelligence");
-            Treats.Items.Add("Speed");
-            Treats.Items.Add("Durability");
+            Traits.Items.Add("Strength");
+            Traits.Items.Add("Intelligence");
+            Traits.Items.Add("Speed");
+            Traits.Items.Add("Durability");
 
             Skills.Items.Add("Sword throw");
             Skills.Items.Add("Magic spell");
@@ -24,6 +26,25 @@ namespace rpg_character_creator
             Races.Items.Add("Reptilian");
             Races.Items.Add("Elf");
             Races.Items.Add("Furry");
+        }
+
+        private void AddTraitButtonClicked(object sender, RoutedEventArgs e)
+        {
+            string trait = (string)Traits.SelectedItem;
+            TraitListView.Items.Add(trait);
+        }        
+        private void RemoveTraitButtonClicked(object sender, RoutedEventArgs e)
+        {
+            TraitListView.Items.Remove(Traits.SelectedItem);
+        }
+        private void AddSkillButtonClicked(object sender, RoutedEventArgs e)
+        {
+            string skill = (string)Skills.SelectedItem;
+            SkillListView.Items.Add(skill);
+        }
+        private void RemoveSkillButtonClicked(object sender, RoutedEventArgs e)
+        {
+            SkillListView.Items.Remove(Skills.SelectedItem);
         }
     }
 }
